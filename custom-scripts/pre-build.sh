32310e1d423d6892333689c6f9b5aa2549295042
Giovani <53870064+GiovaniCancherini@users.noreply.github.com>
2025-08-23 21:53:33 +0000

    feat: Add hello world script and integrate with pre-build process

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -4,4 +4,7 @@
 if [ -d "$BASE_DIR" ]; then
     cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
     chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+
+    cp $BASE_DIR/../custom-scripts/S50hello-config $BASE_DIR/target/etc/init.d
+    chmod +x $BASE_DIR/target/etc/init.d/S50hello-config
 fi
